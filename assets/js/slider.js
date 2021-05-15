8b27baab31cea29ede77704a05a666e8bd9c9eb2
Romanisback <ceo@whiterussia.ru>
2021-05-16 02:12:41 +0300

    BROKEN SLIDER

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,18 +1,44 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    const images = document.querySelectorAll('.slider .slider-container a img');
-    const sliderContainer = document.querySelector('.slider-container');
+//BROKEN
+    // const images = document.querySelectorAll('.slider .slider-container img');
+    // const sliderContainer = document.querySelector('.slider-container');
     
-    let count = 0;
-    let width;
+    // let count = 0;
+    // let width;
     
-    function init() {
-        console.log('resize');
-        width = document.querySelector('.slider').offsetWidth;
-        console.log(width);
-    
-    }
-    
-    init();
+    // function init() {
+    //     console.log('resize');
+    //     width = document.querySelector('.slider').offsetWidth;
+    //     sliderContainer.style.width = width * images.length + 'px';
+    //     images.forEach(item =>{
+    //         item.style.width = width + 'px';
+    //         item.style.height = 'auto';
+    //     })
+      
+    //     rollSlider();
+    // }
+
+    // window.addEventListener('resize', init);
+
+    // init();
+    // document.querySelector('.slider-prev').addEventListener('click', function(){
+    //     count--;
+    //     if(count<0 ){
+    //         count = images.length - 1;
+    //     }
+    //     rollSlider();
+    // })
 
 
-})
+    // document.querySelector('.slider-next').addEventListener('click', function(){
+    //     count++;
+    //     if(count>= images.length){
+    //         count = 0;
+    //     }
+    //     rollSlider();
+    // })
+
+    // function rollSlider(){
+    //     sliderContainer.style.transform = 'translate(-' + count * width + 'px';
+    // }
+    
+
