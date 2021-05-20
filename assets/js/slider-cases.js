24fa2dc6626cc40992eeb5b9c722e9971facc99a
Romanisback <ceo@whiterussia.ru>
2021-05-20 14:42:38 +0300

    slide write part 2

--- assets/js/slider-cases.js
+++ assets/js/slider-cases.js
@@ -1,39 +1,38 @@
 $(document).ready(function(){
     $('.cases-slider').slick({
+        rows: 2,
+        slidesPerRow: 3,
+        fade: false,
+        adaptiveHeight: false,
         infinite: true,
-        speed: 800,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+        pauseOnFocus: true,
+        draggable: true,
+        centerMode: false,
+        variableWidth: false,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        arrows: true,
+        dots: false,
         responsive: [
-        //   {
-        //     breakpoint: 1600,
-        //     settings: {
-        //       slidesToShow: 5,
-        //       slidesToScroll: 1,
-        //       infinite: true,
-        //     }
-        //   },
-          {
-            breakpoint: 992,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 767,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 420,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
+        {
+          breakpoint: 1600,
+          settings: {
+            slidesPerRow: 2,
           }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesPerRow: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            rows: 2,
+            slidesPerRow: 1,
+          }
+        }
         ]
-    })
+      });
 })
