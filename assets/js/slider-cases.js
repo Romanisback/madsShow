0399b2a20096479034325261f1e5f2634f052940
Romanisback <ceo@whiterussia.ru>
2021-05-22 17:49:34 +0300

    add crunchyroll case

--- assets/js/slider-cases.js
+++ assets/js/slider-cases.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
     $('.cases-slider').slick({
-        rows: 2,
-        slidesPerRow: 3,
-        fade: false,
-        adaptiveHeight: false,
-        infinite: true,
-        pauseOnFocus: true,
-        draggable: true,
-        centerMode: false,
-        variableWidth: false,
-        autoplay: false,
-        autoplaySpeed: 5000,
-        arrows: true,
-        dots: false,
-        responsive: [
-        {
-          breakpoint: 1600,
-          settings: {
-            slidesPerRow: 2,
-          }
-        },
-        {
-          breakpoint: 992,
-          settings: {
-            slidesPerRow: 1,
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            rows: 2,
-            slidesPerRow: 1,
-          }
+      rows: 2,
+      slidesPerRow: 3,
+      fade: false,
+      adaptiveHeight: false,
+      infinite: true,
+      pauseOnFocus: true,
+      draggable: true,
+      centerMode: false,
+      variableWidth: false,
+      autoplay: false,
+      autoplaySpeed: 5000,
+      arrows: true,
+      dots: false,
+      responsive: [
+      {
+        breakpoint: 1600,
+        settings: {
+          slidesPerRow: 2,
         }
-        ]
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesPerRow: 1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          rows: 2,
+          slidesPerRow: 1,
+        }
+      }
+      ]
       });
 })
