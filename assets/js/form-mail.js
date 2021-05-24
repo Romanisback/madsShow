a0bed77df8b71895c5b81d0213883d735f7e3379
Romanisback <ceo@whiterussia.ru>
2021-05-24 14:08:54 +0300

    adaptive desk and 1200 device written

--- assets/js/form-mail.js
+++ assets/js/form-mail.js
@@ -40,3 +40,4 @@ $("#sendMail").on("click", function () {
         }
     });
 });
+
---
+++ assets/js/menu-adaptive.js
@@ -0,0 +1,18 @@
+// const w = $(window).width();
+// change_menu_class();
+// $(window).resize(function() {
+//   if ($(window).width()==w) 
+//   w = $(window).width();
+//   change_menu_class();
+//   return;
+// });
+
+// function change_menu_class() {
+//   if ( $(window).width() < 992 ) {
+//     $(".menu").removeClass("menu");
+//     $("nav").addClass("mobile_menu");
+//   } else {
+//     $("nav").removeClass("mobile_menu");
+//     $("nav").addClass("menu");
+//   }
+// }   
